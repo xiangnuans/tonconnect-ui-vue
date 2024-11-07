ac847c2e6e3230675a2bc61db7eef38dc71bbd27
Guo Hong <1352118502@qq.com>
2024-11-07 22:57:09 +0800

    chore: add prettier config

--- src/components/TonConnectButton.vue
+++ src/components/TonConnectButton.vue
@@ -13,25 +13,20 @@ export default defineComponent({
   setup(props, { slots }) {
     const { setOptions } = useTonConnectUI();
 
-    // 组件挂载时调用
     onMounted(() => {
       setOptions({ buttonRootId: props.buttonRootId });
     });
 
-    // 组件卸载时调用
     onBeforeUnmount(() => {
       setOptions({ buttonRootId: null });
     });
 
-    // 返回虚拟节点
     return () => {
-      // 根据 Vue 版本来调整 vnodeData
       const vnodeData = {
         style: { width: "fit-content" },
         ...(isVue2 ? { attrs: { id: props.buttonRootId || "" } } : { id: props.buttonRootId || "" }),
       };
 
-      // 使用 h 函数生成虚拟节点
       return h(
         "div",
         vnodeData,
